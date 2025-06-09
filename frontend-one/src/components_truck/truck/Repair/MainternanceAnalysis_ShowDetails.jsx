fa937c60732da01129e7afd32510248f44ef1967
candycake <candycake96@gmail.com>
2025-06-09 12:57:10 +0700

    2

--- frontend-one/src/components_truck/truck/Repair/MainternanceAnalysis_ShowDetails.jsx
+++ frontend-one/src/components_truck/truck/Repair/MainternanceAnalysis_ShowDetails.jsx
@@ -10,7 +10,7 @@ const MainternanceAnanlysis_ShowDetails = () => {
                     </div>
 
                     <div className="">
-                        <div className="flex-grow-1 me-3" style={{ minWidth: '200px' }}>
+                        <div className="flex-grow-1 me-3 mb-3" style={{ minWidth: '200px' }}>
                             <div className="col-lg-3">
                                 <label htmlFor="reporter" className="form-label mb-1">
                                     ผู้ตรวจเช็ครถ <strong style={{ color: 'red' }}>*</strong>
@@ -26,6 +26,38 @@ const MainternanceAnanlysis_ShowDetails = () => {
                                 />
                             </div>
                         </div>
+                          <div className="row mb-3">
+                                            <div className="col-lg-3 form-check">
+                                                <input
+                                                    className="form-check-input no-disable-style"
+                                                    type="radio"
+                                                    id="availableNow"
+                                                    name="planning_vehicle_availability"
+                                                    value="available"
+                                                    // checked={planning.planning_vehicle_availability === "available"}
+                                                    // onChange={isEditing ? handlePlanningInput : () => { }}
+                                                />
+                                                <label className="form-check-label" htmlFor="availableNow">
+                                                    PM (ซ่อมก่อนเสีย)
+                                                </label>
+                                            </div>
+
+                                            <div className="col-lg-3 form-check">
+                                                <input
+                                                    className="form-check-input no-disable-style"
+                                                    type="radio"
+                                                    id="notAvailable"
+                                                    name="planning_vehicle_availability"
+                                                    value="not_available"
+                                                    // checked={planning.planning_vehicle_availability === "not_available"}
+                                                    // onChange={isEditing ? handlePlanningInput : () => { }}
+                                                />
+                                                <label className="form-check-label" htmlFor="notAvailable">
+                                                    CM (เสียก่อนซ่อม)
+                                                </label>
+                                            </div>
+
+                                        </div>
                     </div>
                 </div>
             </div>
